6a8750f9eba92a6298f4e0f31ecb51b87be7bf81
Ayano-github <96687303+Ayano-github@users.noreply.github.com>
2021-12-26 21:57:47 +0900

    Add files via upload

---
+++ python6 .py
@@ -0,0 +1,50 @@
+"Hello World"
+"Hello World"
+print "Hello World"
+x="John"
+print "I am" + x + "."
+print "I am {}." .format ("John")
+x="John"
+y="Yoko"
+print "I am {}.You are {}.".format (x,y)
+x="john"
+y=58
+print "I am {}, {} years old.".format (x,y)
+x="John"
+y="tokyo"
+print "this is {0}.{1}is {0}'s hometown.".format(x,y)
+
+x="john"
+print "I am %s." %x
+
+x="john"
+y="yoko"
+print "I am %s.You are %s." %(x,y)
+
+x="john"
+y ="yoko"
+print "I am %s.You are %s." %(y,x)
+
+x="John"
+y=28
+
+print "I am %s %d years old." %(x,y)
+
+x="Ayano"
+y="22"
+z="kamakra"
+print "I am {0}. I am {1} years old.".format(x,y)
+
+
+print "I am happy, I am fine" 
+print "I am happy. /niam fine."
+print "I am happy \nI am fine"
+
+print "THE MAN" .center(30)
+print "John".rjust (30)
+print "My name is John. I live in Tokyo.". ljust(30)
+
+print"""
+HOMETOWNJohnHello.IamJohn.Tokyoismyhometown.Jan202015John
+"""
+
